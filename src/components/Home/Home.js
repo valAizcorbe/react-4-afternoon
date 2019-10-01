861f5c9485f50edf33af019093bf6cb154933671
valAizcorbe <valeria.aizcorbe@hotmail.com>
2019-10-01 15:47:34 -0600

    done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,7 @@ export default class Home extends Component {
         <Link to='/classlist/MATH1010'>
           <button className='btn'>Math 1010</button>
         </Link>
-        <Link to='/classlist/ENG2020'>
+        <Link to='/classlist/ENG2010'>
           <button className='btn'>English 2010</button>
         </Link>
         <Link to='/classlist/BIO2020'>
